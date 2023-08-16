ef238798d47f46dcb10ef99937876e8b09315c49
Kumar Hrithik <hrithikk2765@gmail.com>
2023-08-17 04:15:53 +0530

    almost there

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import Body from "./src/components/Body";
 const AppLayout = () => {
   return (
     <>
-      <h1>Hello world</h1>
+      <Body />
     </>
   );
 };
